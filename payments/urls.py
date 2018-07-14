6487d3a4265bb85bea02520efa4ef5cc4518df1d
Cody Bontecou <bontecouc@gmail.com>
2018-07-14 08:30:58 -1000

    Organized url format

--- payments/urls.py
+++ payments/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-
 from .views import home_page_view, login_view, charge_view, registration_view, registration_redirect, logout_view
 
 urlpatterns = [
     path('', home_page_view, name='home'),
     path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
     path('registration', registration_view, name='registration'),
     path('registration/redirect', registration_redirect, name='registration_redirect'),
-    path('logout', logout_view, name='logout'),
-    path('charge', charge_view, name='charge'),
 ]
