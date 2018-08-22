3928ad85c61d51aaed5c4ba98875cf08fb006cc2
Cody Bontecou <bontecouc@gmail.com>
2018-08-22 15:50:35 -0600

    test_model tests for get_full_name method

--- user_authentication/test_models.py
+++ user_authentication/test_models.py
@@ -5,6 +5,10 @@ from .models import User
 
 class UserModelTest(TestCase):
 
-    def test_string_representation(self):
+    def test_string_representation_of_user_model(self):
         entry = User(email='test@gmail.com', full_name='testable full_name')
         self.assertEqual(str(entry), entry.email)
+
+    def test_user_model_get_full_name_method(self):
+        entry = User(email='test@gmail.com', full_name='testable full_name')
+        self.assertEqual(entry.get_full_name(), 'testable full_name')
