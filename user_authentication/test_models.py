5ca81768d79409ba698f52ed7890888f6116e361
Cody Bontecou <bontecouc@gmail.com>
2018-08-24 11:19:32 -0600

    test for User model's has_perm method

--- user_authentication/test_models.py
+++ user_authentication/test_models.py
@@ -13,6 +13,10 @@ class TestUserModel(TestCase):
         entry = User(email='test@gmail.com', full_name='testable full_name')
         self.assertEqual(entry.get_full_name(), 'testable full_name')
 
+    def test_user_model_get_full_name_method_returning_email(self):
+        entry = User(email='test@gmail.com')
+        self.assertEqual(entry.get_full_name(), 'test@gmail.com')
+
     def test_user_model_get_short_name_method(self):
         entry = User(email='test@gmail.com', full_name='testable full_name')
         self.assertEqual(entry.get_short_name(), 'testable')
@@ -20,4 +24,10 @@ class TestUserModel(TestCase):
     def test_user_model_get_short_name_method_returning_email(self):
         entry = User(email='test@gmail.com', full_name='testablefull_name')
         self.assertEqual(entry.get_short_name(), 'test@gmail.com')
+
+    def test_has_perm_returns_true(self):
+        entry = User(email='test@gmail.com', full_name='testable full_name')
+        self.assertTrue(entry.has_perm(perm='test'))
+
+
 
