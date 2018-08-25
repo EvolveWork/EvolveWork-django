3c2d4d720c83fb16d96d267f0e05867615e766a4
Cody Bontecou <bontecouc@gmail.com>
2018-08-25 15:55:05 -0600

    Able to get some progress on stripe_charge tests_views

--- stripe_charge/test_views.py
+++ stripe_charge/test_views.py
@@ -1,6 +1,12 @@
+import datetime
+from unittest import mock
+
+import stripe
 from django.test import TestCase
 from django.urls import reverse
+from stripe.error import CardError
 
+from evolve_work import settings
 from user_authentication.models import User
 
 
@@ -15,12 +21,78 @@ class TestChargeSuccessView(TestCase):
         self.assertTemplateUsed(response, 'charge_success.html')
 
 
-class TestAccountView(TestCase):
+class TestCheckoutViewLoggedOut(TestCase):
+    def test_checkout_view(self):
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 302)
+
+
+class TestCheckoutView(TestCase):
 
     def setUp(self):
         User.objects.create_user(email='test@gmail.com', full_name='testable full_name', password='testing_test_pw')
         self.client.login(email='test@gmail.com', password='testing_test_pw')
 
+    def test_checkout_with_incorrect_card(self):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        self.assertRaises(stripe.error.CardError, stripe.Token.create, card={
+                'number': '4242424242424241',
+                'exp_month': '6',
+                'exp_year': str(datetime.datetime.today().year + 1),
+                'cvc': '123',
+            })
+
     def test_charge_checkout_view(self):
-        response = self.client.get(reverse('account'))
-        print(response)
+        """These two tests must live together because they need to be done sequentially.
+
+        First, we place a donation using the client. Then we send a mock callback to our
+        webhook, to make sure it accepts it properly.
+        """
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        # Create a stripe token (this would normally be done via javascript in the front
+        # end when the submit button was pressed)
+        token = stripe.Token.create(
+            card={
+                'number': '4242424242424242',
+                'exp_month': '6',
+                'exp_year': str(datetime.datetime.today().year + 1),
+                'cvc': '123',
+            }
+        )
+
+        # Place a donation as an anonymous (not logged in) person using the
+        # token we just got
+        r = self.client.post(reverse('checkout'), data={
+            'amount': '25',
+            'payment_provider': 'cc',
+            'first_name': 'Barack',
+            'last_name': 'Obama',
+            'address1': '1600 Pennsylvania Ave.',
+            'address2': 'The Whitehouse',
+            'city': 'DC',
+            'state': 'DC',
+            'zip_code': '20500',
+            'email': 'barack@freelawproject.org',
+            'referrer': 'footer',
+            'stripeToken': token.id,
+        })
+
+        self.assertEqual(r.status_code, 302)  # 302 because we redirect after a post.
+
+        # # Get the stripe event so we can post it to the webhook
+        # # We don't know the event ID, so we have to get the latest ones, then filter...
+        # events = stripe.Event.all()
+        # event = None
+        # for obj in events.data:
+        #     if obj.data.object.card.fingerprint == token.card.fingerprint:
+        #         event = obj
+        #         break
+        # self.assertIsNotNone(event, msg="Unable to find correct event for token: %s" % token.card.fingerprint)
+        #
+        # # Finally, we can test the webhook!
+        # r = self.client.post('/donate/callbacks/stripe/',
+        #                      data=simplejson.dumps(event),
+        #                      content_type='application/json')
+        #
+        # # Does it return properly?
+        # self.assertEqual(r.status_code, 200)
--- user_authentication/test_forms.py
+++ user_authentication/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .forms import SignupForm
+from .forms import SignupForm, UserAdminCreationForm
 
 
 class TestSignUpForm(TestCase):
@@ -20,3 +20,15 @@ class TestSignUpForm(TestCase):
     def test_blank_data_in_SignUpForm(self):
         form = SignupForm({})
         self.assertFalse(form.is_valid())
+
+
+class TestUserAdminCreationForm(TestCase):
+
+    def test_clean_password2(self):
+        form = UserAdminCreationForm({
+            'email': 'test_signupform@gmail.com',
+            'full_name': 'test signupform',
+            'password1': 'testing_test_pw',
+            'password2': 'testing_test_pw'
+        })
+        self.assertEqual(form.clean_password2(), 'testing_test_pw')
