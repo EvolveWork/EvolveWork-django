b378e4753fa2504799ace8e68a23aa43d42b41a5
Cody Bontecou <bontecouc@gmail.com>
2018-09-07 09:15:15 -0600

    trimmed amount of code in test_charge_checkout_view. Failing on assertTemplateUsed

--- stripe_charge/test_views.py
+++ stripe_charge/test_views.py
@@ -32,8 +32,9 @@ class TestCheckoutViewLoggedOut(TestCase):
 class TestCheckoutView(TestCase):
 
     def setUp(self):
-        User.objects.create_user(email='test@gmail.com', full_name='testable full_name', password='testing_test_pw')
-        self.client.login(email='test@gmail.com', password='testing_test_pw')
+        self.user = User.objects.create_user(email='test@gmail.com', full_name='testable full_name',
+                                             password='testing_test_pw')
+        self.client.login(email=self.user.email, password=self.user.password)
 
     def test_checkout_with_incorrect_card(self):
         try:
@@ -52,39 +53,29 @@ class TestCheckoutView(TestCase):
 
         stripe.api_key = settings.STRIPE_SECRET_KEY
 
-        try:
-            token = stripe.Token.create(
-                card={
-                    'number': '4242424242424242',
-                    'exp_month': '6',
-                    'exp_year': str(datetime.datetime.today().year + 1),
-                    'cvc': '123',
-                }
-            )
-            response = self.client.post(reverse('checkout'), data={
-                'amount': '25',
-                'payment_provider': 'cc',
-                'first_name': 'Barack',
-                'last_name': 'Obama',
-                'address1': '1600 Pennsylvania Ave.',
-                'address2': 'The Whitehouse',
-                'city': 'DC',
-                'state': 'DC',
-                'zip_code': '20500',
-                'email': 'barack@freelawproject.org',
-                'referrer': 'footer',
-                'stripeToken': token.id,
-            })
-            self.assertEqual(response.status_code, 302)
+        # try:
+        token = stripe.Token.create(
+            card={
+                'number': '4242424242424242',
+                'exp_month': '6',
+                'exp_year': str(datetime.datetime.today().year + 1),
+                'cvc': '123',
+            }
+        )
+        response = self.client.post(reverse('checkout'), data={
+            'stripeToken': token.id,
+        })
+        self.assertTemplateUsed(response, 'charge_success.html')
 
-        except Exception:
-            self.fail('Stripe api may be down.')
+    # except Exception:
+    #     self.fail('Stripe api may be down.')
 
 
 class TestCancelSubscription(TestCase):
 
     def setUp(self):
-        User.objects.create_user(email='test@gmail.com', full_name='testable full_name', password='testing_test_pw')
+        self.user = User.objects.create_user(email='test@gmail.com', full_name='testable full_name',
+                                             password='testing_test_pw')
         self.client.login(email='test@gmail.com', password='testing_test_pw')
 
     def test_cancel_subscription_template(self):
@@ -100,4 +91,3 @@ class TestCancelSubscription(TestCase):
 
         response = self.client.get(reverse('cancel_subscription_complete'))
         self.assertTemplateUsed(response, 'charge_cancel_complete.html')
-
