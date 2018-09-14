82b71fb9f07e31bb2a7c48cc54ec19d08a653851
Cody Bontecou <bontecouc@gmail.com>
2018-09-14 09:25:52 -0600

    moved comment for formatting

--- stripe_charge/urls.py
+++ stripe_charge/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import charge_view, checkout, charge_success, cancel_subscription, cancel_subscription_complete, \
-    renew_subscription, renew_subscription_complete
+from .views import charge_view, checkout, charge_success, cancel_subscription, cancel_subscription_complete
+# renew_subscription, renew_subscription_complete
 
 urlpatterns = [
     path('', charge_view, name='charge'),
@@ -9,7 +9,7 @@ urlpatterns = [
     path('success/', charge_success, name="charge_success"),
     path('cancel/', cancel_subscription, name='cancel_subscription'),
     path('cancel/complete/', cancel_subscription_complete, name='cancel_subscription_complete'),
-    path('renew/', renew_subscription, name='renew_subscription'),
-    path('renew/complete/', renew_subscription_complete, name='renew_subscription_complete')
+    # path('renew/', renew_subscription, name='renew_subscription'),
+    # path('renew/complete/', renew_subscription_complete, name='renew_subscription_complete')
     # path('register_and_checkout/', register_and_checkout, name='register_and_checkout'),
 ]
